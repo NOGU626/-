daea28dd7686e1aa7e287e8bd659ed94198cdc18
野口大輝 <noguchi@cps.im.dendai.ac.jp>
2021-05-10 17:20:25 +0900

    APIドキュメントの書き方をミスっていたので修正

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-from flasgger import Swagger
+from flasgger import Swagger , swag_from
 
 import os
 
@@ -135,38 +135,27 @@ def handle_message(event):
         TextSendMessage(text=event.message.text))  # ここでオウム返しのメッセージを返します。
 
 
+
 @app.route('/colors/<palette>/')
 def colors(palette):
-    """Example endpoint returning a list of colors by palette
-    This is using docstrings for specifications.
-    ---
-    parameters:
-      - name: palette
-        in: path
-        type: string
-        enum: ['all', 'rgb', 'cmyk']
-        required: true
-        default: all
-    definitions:
-      Palette:
-        type: object
-        properties:
-          palette_name:
-            type: array
-            items:
-              $ref: '#/definitions/Color'
-      Color:
-        type: string
-    responses:
-      200:
-        description: A list of colors (may be filtered by palette)
-        schema:
-          $ref: '#/definitions/Palette'
-        examples:
-          rgb: ['red', 'green', 'blue']
-    security:
-      - JWTtoken: []
     """
+        post user
+        ---
+        tags:
+          - user
+        produces:
+          - application/json
+        parameters:
+          - in: body
+            name: user
+            schema:
+              $ref: '#/definitions/UserSchema'
+        responses:
+          200:
+            description: Successful operation
+            schema:
+              $ref: "#/definitions/UserSchema"
+        """
     all_colors = {
         'cmyk': ['cian', 'magenta', 'yellow', 'black'],
         'rgb': ['red', 'green', 'blue']
